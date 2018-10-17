43e9be9ad75a51de5f26f80b78afa71ac4b9148b
Dmitry Volovod <dimazoll@shri2018.yaconnect.com>
2018-10-17 17:37:55 +0300

    add getters and setters to VideoModel class that updates view

--- src/js/VideoModel.js
+++ src/js/VideoModel.js
@@ -1,8 +1,38 @@
 export default class VideoModel {
   constructor(domNode) {
-    this.brightness = 1.0;
-    this.contrast = 1.0;
-    this.volume = 1.0;
+    this._brightness = 1.0;
+    this._contrast = 1.0;
+    this._volume = 1.0;
     this.domNode = domNode;
+  }
+
+  updateFilters() {
+    this.domNode.style.filter = `brightness(${this._brightness}) contrast(${this._contrast})`;
+  }
+
+  get brightness() {
+    return Math.floor(this._brightness * 100);
+  }
+
+  set brightness(value) {
+    this._brightness = value / 100;
+    this.updateFilters();
+  }
+
+  get contrast() {
+    return Math.floor(this._contrast * 100);
+  }
+
+  set contrast(value) {
+    this._contrast = value / 100;
+    this.updateFilters();
+  }
+
+  get volume() {
+    return Math.floor(this._volume * 100);
+  }
+
+  set volume(value) {
+    this._volume = value / 100;
   }
 }
